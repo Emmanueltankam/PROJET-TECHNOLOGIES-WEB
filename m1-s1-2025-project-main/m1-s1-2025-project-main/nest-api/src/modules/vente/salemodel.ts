1531a2d2eaf09232db1c5dd3b6995128d4fc874c
BriceSandjong <brice-verges.sandjong-mbiada@student.junia.com>
2025-11-04 09:34:11 +0100

    rectification des erreurs d'acces a la bd en rendant les images nullable et suppression du module vente pour le refaire plus tard

--- m1-s1-2025-project-main/m1-s1-2025-project-main/nest-api/src/modules/vente/salemodel.ts
+++
@@ -1,6 +0,0 @@
-export type SaleModel = {
-    id: string;          // Identifiant unique de la vente
-    clientId: string;   // Identifiant du client
-    bookId: string;     // Identifiant du livre
-    date: string;       // Date d'achat (format ISO)
-};
